a817a3b3542108fff81495b11a2aa6cadb872ad8
igorbeilner <igor.beilner@hotmail.com>
2015-06-12 23:53:08 -0300

    insere

---
+++ Fonte/insert.c
@@ -0,0 +1,25 @@
+#include "buffend.h"
+
+
+int insere(insert *nomeTabela) {
+
+	int i;
+	table *tabela = (table *)malloc(sizeof(tabela));
+	column *colunas = NULL;
+	struct fs_objects objeto;
+
+	abreTabela(nomeTabela->tableName, &objeto, &tabela->esquema);
+	strcpy(tabela->nome, nomeTabela->tableName);
+
+	for(i=0; i < nomeTabela->N; i++) {
+		colunas = insereValor(tabela, colunas, nomeTabela->columnName[i], nomeTabela->values[i]);
+	}
+
+	if(finalizaInsert(nomeTabela->tableName, colunas) == SUCCESS) {
+		printf("INSERT 0 1\n");
+	} 
+
+
+
+    return 0;
+}
