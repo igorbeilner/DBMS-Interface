e92a44031f0cecf41f0f93b243c01eb80f2400ea
igorbeilner <igor.beilner@hotmail.com>
2015-06-13 01:34:36 -0300

    codigo elegante

--- Fonte/insert.c
+++ Fonte/insert.c
@@ -11,25 +11,15 @@ int insert(rc_insert *s_insert) {
 	abreTabela(s_insert->tableName, &objeto, &tabela->esquema); //retorna o esquema para a insere valor
 	strcpy(tabela->nome, s_insert->tableName);
 
-	if(s_insert->columnName != NULL) {
-
-		for(i=0; i < s_insert->N; i++) {
+	if(s_insert->columnName != NULL)
+		for(i=0; i < s_insert->N; i++)
 			colunas = insereValor(tabela, colunas, s_insert->columnName[i], s_insert->values[i]);
-		}
-
-	} else {
-
-		for(i=0; i < objeto.qtdCampos; i++) {
+	else
+		for(i=0; i < objeto.qtdCampos; i++)
 			colunas = insereValor(tabela, colunas, tabela->esquema[i].nome, s_insert->values[i]);
-		}
 
-	}
-
-	if(finalizaInsert(s_insert->tableName, colunas) == SUCCESS) {
+	if(finalizaInsert(s_insert->tableName, colunas) == SUCCESS)
 		printf("INSERT 0 1\n");
-	} 
-
-
 
     return 0;
 }
