aa2772487fba47c572e09b33ca1e18640b845b78
igorbeilner <igor.beilner@hotmail.com>
2015-06-13 01:25:51 -0300

    insert sem o nome das colunas

--- Fonte/insert.c
+++ Fonte/insert.c
@@ -8,18 +8,18 @@ int insert(rc_insert *s_insert) {
 	column *colunas = NULL;
 	struct fs_objects objeto;
 
-	abreTabela(s_insert->tableName, &objeto, &tabela->esquema);
+	abreTabela(s_insert->tableName, &objeto, &tabela->esquema); //retorna o esquema para a insere valor
 	strcpy(tabela->nome, s_insert->tableName);
 
 	if(s_insert->columnName != NULL) {
-		
+
 		for(i=0; i < s_insert->N; i++) {
 			colunas = insereValor(tabela, colunas, s_insert->columnName[i], s_insert->values[i]);
 		}
 
 	} else {
 
-		for(i=0; i < s_insert->N; i++) {
+		for(i=0; i < objeto.qtdCampos; i++) {
 			colunas = insereValor(tabela, colunas, tabela->esquema[i].nome, s_insert->values[i]);
 		}
 
