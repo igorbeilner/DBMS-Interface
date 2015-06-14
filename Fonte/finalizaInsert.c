14f1dbca767009d84314832847a40bd62a88324f
Regis Thiago Feyh <registhiagofeyh@gmail.com>
2015-06-14 18:50:11 -0300

    Melhoria das mensagens de erro.

--- Fonte/finalizaInsert.c
+++ Fonte/finalizaInsert.c
@@ -11,11 +11,11 @@ int finalizaInsert(char *nome, column *c){
     auxT = abreTabela(nome, &dicio, &auxT);
 
     table *tab     = (table *)malloc(sizeof(table));
-    tp_table *tab2 = (tp_table *)malloc(sizeof(struct tp_table)); 
+    tp_table *tab2 = (tp_table *)malloc(sizeof(struct tp_table));
 
     tab->esquema = abreTabela(nome, &objeto, &tab->esquema);
     tab2 = procuraAtributoFK(objeto);
-    
+
     for(j = 0, temp = c; j < objeto.qtdCampos && temp != NULL; j++, temp = temp->next){
         switch(tab2[j].chave){
             case NPK:
@@ -25,13 +25,13 @@ int finalizaInsert(char *nome, column *c){
             case PK:
                 erro = verificaChavePK(nome, temp , temp->nomeCampo, temp->valorCampo);
                 if(erro == ERRO_CHAVE_PRIMARIA){
-                    printf("Erro GRAVE! na função verificaChavePK(). Erro de Chave Primaria.\nAbortando...\n");
+                    printf("Erro de chave primária. ('%s' duplicado: '%s')\n", temp->nomeCampo, temp->valorCampo);
                     free(c);    // Libera a memoria da estrutura.
 					free(auxT); // Libera a memoria da estrutura.
-					//free(temp); // Libera a memoria da estrutura.   
+					//free(temp); // Libera a memoria da estrutura.
 					free(tab); // Libera a memoria da estrutura.
 					free(tab2); // Libera a memoria da estrutura.
-                    exit(1);
+                    return ERRO_CHAVE_PRIMARIA;
                 }
 
                 break;
@@ -42,51 +42,51 @@ int finalizaInsert(char *nome, column *c){
                     erro = verificaChaveFK(nome, temp, tab2[j].nome, temp->valorCampo, tab2[j].tabelaApt, tab2[j].attApt);
 
                     if(erro != SUCCESS){
-                        printf("Erro GRAVE! na função verificaChaveFK(). Erro de Chave Estrangeira.\nAbortando...\n");
+                        printf("Erro de chave estrangeira. (Não existe uma chave primária '%s.%s' com o valor '%s')\n", tab2[j].tabelaApt,tab2[j].attApt,temp->valorCampo);
                         free(c);    // Libera a memoria da estrutura.
 						free(auxT); // Libera a memoria da estrutura.
 						free(temp); // Libera a memoria da estrutura.
                         free(tab); // Libera a memoria da estrutura.
 						free(tab2); // Libera a memoria da estrutura.
-                        exit(1);
+                        return ERRO_CHAVE_ESTRANGEIRA;
                     }
                 }
 
                 break;
         }
     }
-    
-    
+
+
     if(erro == ERRO_CHAVE_ESTRANGEIRA){
-        printf("Erro GRAVE! na função verificaChaveFK(). Erro de Chave Estrangeira.\nAbortando...\n");
+        printf("Erro de chave estrangeira.\n");
         free(c);    // Libera a memoria da estrutura.
 		free(auxT); // Libera a memoria da estrutura.
 		free(temp); // Libera a memoria da estrutura.
         free(tab); // Libera a memoria da estrutura.
 		free(tab2); // Libera a memoria da estrutura.
-        exit(1);
+        return ERRO_CHAVE_ESTRANGEIRA;
     }
 
     if(erro == ERRO_CHAVE_PRIMARIA){
-        printf("Erro GRAVE! na função verificaChavePK(). Erro de Chave Primaria.\nAbortando...\n");
+        printf("Erro de chave Primaria.");
         free(c);    // Libera a memoria da estrutura.
 		free(auxT); // Libera a memoria da estrutura.
 		free(temp); // Libera a memoria da estrutura.
         free(tab); // Libera a memoria da estrutura.
 		free(tab2); // Libera a memoria da estrutura.
-        exit(1);
+        return ERRO_CHAVE_PRIMARIA;
     }
     if(erro == ERRO_DE_PARAMETRO) {
-        printf("Erro GRAVE! na função finalizaInsert(). Erro de Parametro.\nAbortando...\n");
+        printf("Erro na função finalizaInsert(). Erro de parametro.\n");
         free(c);    // Libera a memoria da estrutura.
 		free(auxT); // Libera a memoria da estrutura.
 		free(temp); // Libera a memoria da estrutura.
         free(tab); // Libera a memoria da estrutura.
 		free(tab2); // Libera a memoria da estrutura.
-        exit(1);
+        return ERRO_DE_PARAMETRO;
     }
-    
-    
+
+
     if((dados = fopen(dicio.nArquivo,"a+b")) == NULL){
 		free(c);    // Libera a memoria da estrutura.
 		free(auxT); // Libera a memoria da estrutura.
@@ -94,15 +94,15 @@ int finalizaInsert(char *nome, column *c){
         free(tab); // Libera a memoria da estrutura.
 		free(tab2); // Libera a memoria da estrutura.
         return ERRO_ABRIR_ARQUIVO;
-        
+
 	}
-    
+
     for(auxC = c, t = 0; auxC != NULL; auxC = auxC->next, t++){
         if(t >= dicio.qtdCampos)
             t = 0;
 
         if(auxT[t].tipo == 'S'){ // Grava um dado do tipo string.
-            
+
             if(sizeof(auxC->valorCampo) > auxT[t].tam && sizeof(auxC->valorCampo) != 8){
 				free(tab); // Libera a memoria da estrutura.
 				free(tab2); // Libera a memoria da estrutura.
@@ -129,7 +129,7 @@ int finalizaInsert(char *nome, column *c){
         else if(auxT[t].tipo == 'I'){ // Grava um dado do tipo inteiro.
             i = 0;
             while (i < strlen(auxC->valorCampo)){
-                if(auxC->valorCampo[i] < 48 || auxC->valorCampo[i] > 57){ 
+                if(auxC->valorCampo[i] < 48 || auxC->valorCampo[i] > 57){
 					free(tab); // Libera a memoria da estrutura.
 					free(tab2); // Libera a memoria da estrutura.
 					free(c);    // Libera a memoria da estrutura.
@@ -142,14 +142,14 @@ int finalizaInsert(char *nome, column *c){
             }
 
             int valorInteiro = 0;
-            
+
             sscanf(auxC->valorCampo,"%d",&valorInteiro);
             fwrite(&valorInteiro,sizeof(valorInteiro),1,dados);
         }
         else if(auxT[t].tipo == 'D'){ // Grava um dado do tipo double.
             x = 0;
             while (x < strlen(auxC->valorCampo)){
-                if((auxC->valorCampo[x] < 48 || auxC->valorCampo[x] > 57) && (auxC->valorCampo[x] != 46)){ 
+                if((auxC->valorCampo[x] < 48 || auxC->valorCampo[x] > 57) && (auxC->valorCampo[x] != 46)){
 					free(tab); // Libera a memoria da estrutura.
 					free(tab2); // Libera a memoria da estrutura.
 					free(c);    // Libera a memoria da estrutura.
@@ -177,11 +177,11 @@ int finalizaInsert(char *nome, column *c){
                 return ERRO_NO_TIPO_CHAR;
             }
             char valorChar = auxC->valorCampo[0];
-            fwrite(&valorChar,sizeof(valorChar),1,dados);           
+            fwrite(&valorChar,sizeof(valorChar),1,dados);
         }
 
     }
-    
+
     fclose(dados);
     free(tab); // Libera a memoria da estrutura.
 	free(tab2); // Libera a memoria da estrutura.
