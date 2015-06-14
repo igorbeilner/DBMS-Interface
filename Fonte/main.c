645f101a0780aaca5460e4e613870283d7ec0230
elitont <eliton_traverssini@outlook.com>
2015-06-14 14:12:38 -0300

    nova main

---
+++ Fonte/main.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <string.h>
+#include "buffend.h"
+#include "interface/y.tab.h"
+
+
+int main(){
+    int nrTabelas = 3;
+    int nTabela[nrTabelas];
+    table  *tab[nrTabelas];
+    column *colunas;
+    int object, schema;
+
+    object      = existeArquivo("fs_object.dat");
+    schema      = existeArquivo("fs_schema.dat");
+    nTabela[0]  = existeArquivo("Aluno.dat");
+    nTabela[1]  = existeArquivo("Inst.dat");
+    nTabela[2]  = existeArquivo("Inscri.dat");
+     if(!object || !schema){
+
+        if(!nTabela[0]){                                                                    //Se ainda não existe a Aluno, a mesma é criada
+            tab[0] = iniciaTabela("Aluno");                                                 //Cria a tabela
+            tab[0] = adicionaCampo(tab[0], "CPF"     , 'I', (sizeof(int))   ,PK,"","");     //Cria os atributos
+            tab[0] = adicionaCampo(tab[0], "Nome"    , 'S', 20              ,NPK,"","");
+            tab[0] = adicionaCampo(tab[0], "Endereco", 'S', 20              ,NPK,"","");
+            tab[0] = adicionaCampo(tab[0], "Peso"    , 'D', (sizeof(double)),NPK,"","");
+            finalizaTabela(tab[0]);
+         }
+         if(!nTabela[1]){
+            tab[1] = iniciaTabela("Inst");
+            tab[1] = adicionaCampo(tab[1], "CodInst"  , 'I', (sizeof(int))   ,PK , "","");
+            tab[1] = adicionaCampo(tab[1], "Nome"     , 'S', 20              ,NPK, "","");
+            tab[1] = adicionaCampo(tab[1], "Endereco" , 'S', 20              ,NPK, "","");
+            tab[1] = adicionaCampo(tab[1], "Reitor"   , 'S', 10              ,NPK, "","");
+            finalizaTabela(tab[1]);
+        }
+        if(!nTabela[2]){
+            tab[2] = iniciaTabela("Inscri");
+            tab[2] = adicionaCampo(tab[2], "CodMat"     , 'I', (sizeof(int))  ,PK, "","");
+            tab[2] = adicionaCampo(tab[2], "CPF"        , 'I', (sizeof(int))  ,FK, "Aluno","CPF");
+            tab[2] = adicionaCampo(tab[2], "CodInst"    , 'I', (sizeof(int))  ,FK , "Inst","CodInst");
+            tab[2] = adicionaCampo(tab[2], "Curso"   , 'S',  20  ,NPK, "","");
+            finalizaTabela(tab[2]);
+        }
+
+        printf("Conectando...\n");
+        printf("Conectado!\n"); // hahaha
+        interface();
+        printf("Desconectado.\n");
+        // INSERT INTO Aluno (CPF, Nome, Endereco, Peso) VALUES (123456, Rogerio, RuaMarechal, 80);
+        // INSERT INTO Aluno VALUES (123456, Rogerio, RuaMarechal, 80);
+
+
+        //Inserção de tuplas na tabela2
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "111");
+        colunas = insereValor(tab[1],colunas, "Nome", "UFFS");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadeTerra");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaChuva");
+        finalizaInsert("Inst", colunas);
+
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "222");
+        colunas = insereValor(tab[1],colunas, "Nome", "CEFET");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadePedra");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaVento");
+        finalizaInsert("Inst", colunas);
+
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "333");
+        colunas = insereValor(tab[1],colunas, "Nome", "UNOESC");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadeAsfal");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaAgua");
+        finalizaInsert("Inst", colunas);
+
+
+
+        //Inserção de tupla na tabela3
+        colunas = NULL;
+        colunas = insereValor(tab[2],colunas, "CodMat", "1401");
+        colunas = insereValor(tab[2],colunas, "CPF", "123456");
+        colunas = insereValor(tab[2],colunas, "CodInst", "333");
+        colunas = insereValor(tab[2],colunas, "Curso", "CC");
+        finalizaInsert("Inscri", colunas);
+        
+    }
+
+
+    imprime("Aluno");           //Imprime os atributos da tabela
+    imprime("Inst");
+    imprime("Inscri");
+    excluirTabela("Inscri");    //Exclui os dados da tabela do dicionario e remove-a do disco
+    excluirTabela("Inst");
+    excluirTabela("Aluno");
+
+
+    return 0;
+}
