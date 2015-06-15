058933ec3f37f2af8061ef945806baae70915ca6
Regis Thiago Feyh <registhiagofeyh@gmail.com>
2015-06-15 00:01:36 -0300

    Insert aceitando comandos sem todas as colunas e em ordens diferentes.

--- Fonte/insert.c
+++ Fonte/insert.c
@@ -1,39 +1,74 @@
 #include "buffend.h"
 
+int typesCompatible(char table_type, char insert_type) {
+	return (table_type == 'D' && insert_type == 'I')
+		|| (table_type == 'S' && insert_type == 'I')
+		|| (table_type == 'C' && insert_type == 'I')
+		|| (table_type == 'S' && insert_type == 'D')
+		|| (table_type == 'S' && insert_type == 'C')
+		|| (table_type == 'D' && insert_type == 'D')
+		|| (table_type == 'I' && insert_type == 'I')
+		|| (table_type == 'S' && insert_type == 'S')
+		|| (table_type == 'C' && insert_type == 'C');
+
+}
+
+// Busca o valor da coluna. Assume que columnName foi preenchido.
+char *getInsertedValue(rc_insert *s_insert, char *columnName, table *tabela) {
+	int i;
+	char tipo, *noValue;
+
+	for (i = 0; i < s_insert->N; i++) {
+		if (strcmp(s_insert->columnName[i], columnName) == 0) {
+			return s_insert->values[i];
+		}
+	}
+
+	tipo = retornaTamanhoTipoDoCampo(columnName, tabela);
+	noValue = (char *)malloc(sizeof(char)*3);
+
+	if (tipo == 'I') {
+		noValue = "0";
+	} else if (tipo == 'D') {
+		noValue = "0.0";
+	} else
+		noValue[0] = '\0';
+
+	return noValue;
+}
+
+char getInsertedType(rc_insert *s_insert, char *columnName, table *tabela) {
+	int i;
+	char noValue;
+	for (i = 0; i < s_insert->N; i++) {
+		if (strcmp(s_insert->columnName[i], columnName) == 0) {
+			return s_insert->type[i];
+		}
+	}
+
+	noValue = retornaTamanhoTipoDoCampo(columnName, tabela);
+
+	return noValue;
+}
 
 void insert(rc_insert *s_insert) {
 
 	int i;
 	table *tabela = (table *)malloc(sizeof(table));
 	column *colunas = NULL;
+	tp_table *esquema;
 	struct fs_objects objeto;
-	char type, flag=0;
+	char  flag=0;
 
 	abreTabela(s_insert->tableName, &objeto, &tabela->esquema); //retorna o esquema para a insere valor
 	strcpy(tabela->nome, s_insert->tableName);
 
 	if(s_insert->columnName != NULL) {
-		for(i=0; i < s_insert->N; i++) {
-
-			type = retornaTamanhoTipoDoCampo(s_insert->columnName[i],tabela);
-
-			if(s_insert->type[i] == 'S' && type == 'C') {
-				s_insert->values[i][1] = '\0';
-				s_insert->type[i] = 'C';
-			}
-
-			if(s_insert->type[i] == 'I' && type == 'D') {
-				s_insert->values[i][procuraPonto(s_insert->values[i])] = '\0';
-				s_insert->type[i] = 'D';
-			}
-
-			if(!type) { 		//verifica se a coluna foi encontrada
-				printf("A coluna '%s' não existe na tabela '%s'\n", s_insert->columnName[i], tabela->nome);
-				flag=1;
-			} else if(s_insert->type[i] == type)	//verifica se o dado inserido e do mesmo tipo que o aceito pela coluna
-				colunas = insereValor(tabela, colunas, s_insert->columnName[i], s_insert->values[i]);
-			else {
-				printf("Tipo de dados invalido para a coluna '%s' da tabela '%s' (esperado: %c, recebido: %c)\n", s_insert->columnName[i], tabela->nome, type, s_insert->type[i]);
+		for (esquema = tabela->esquema; esquema != NULL; esquema = esquema->next) {
+			if(typesCompatible(esquema->tipo,getInsertedType(s_insert, esquema->nome, tabela))) {
+				colunas = insereValor(tabela, colunas, esquema->nome, getInsertedValue(s_insert, esquema->nome, tabela));
+			} else {
+				printf("Tipo de dados invalido para a coluna '%s' da tabela '%s' (esperado: %c, recebido: %c)\n", esquema->nome, tabela->nome, esquema->tipo, getInsertedType(s_insert, esquema->nome, tabela));
 				flag=1;
 			}
 		}
@@ -59,6 +94,7 @@ void insert(rc_insert *s_insert) {
 			}
 		}
 	}
+
 	if(!flag)
 		if(finalizaInsert(s_insert->tableName, colunas) == SUCCESS)
 			printf("INSERT 0 1\n");
