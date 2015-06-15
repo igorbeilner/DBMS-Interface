3b93761295fc9981d008f468e3f6a995ed521eb5
Regis Thiago Feyh <registhiagofeyh@gmail.com>
2015-06-14 22:30:06 -0300

    For para verificar se o campo existe. Preenche o tipo se ele existir.

--- Fonte/insert.c
+++ Fonte/insert.c
@@ -6,6 +6,7 @@ void insert(rc_insert *s_insert) {
 	int i;
 	table *tabela = (table *)malloc(sizeof(table));
 	column *colunas = NULL;
+	tp_table *esq;
 	struct fs_objects objeto;
 	char type, flag=0;
 
@@ -15,7 +16,19 @@ void insert(rc_insert *s_insert) {
 	if(s_insert->columnName != NULL) {
 		for(i=0; i < s_insert->N; i++) {
 
-			type = retornaTamanhoTipoDoCampo(s_insert->columnName[i], tabela);
+			printf("----\n");
+			for (esq = tabela->esquema, type = 0; esq != NULL; esq = esq->next) {
+				/*printf("Compara: %s e %s\n", esq->nome, s_insert->columnName[i]);
+				if (strcmp(esq->nome, s_insert->columnName[i]) == 0) {
+					type = esq->tipo;
+					break;
+				}
+				printf("next: %s\n", esq->next->nome);*/
+				printf("%s\n", esq->nome);
+			}
+			printf("-----\n");
+
+			//type = retornaTamanhoTipoDoCampo(s_insert->columnName[i], tabela);
 
 			if(s_insert->type[i] == 'S' && type == 'C') {
 				s_insert->values[i][1] = '\0';
