abf27c574152474a9766609b24b799cc1aecdbd0
Regis Thiago Feyh <registhiagofeyh@gmail.com>
2015-07-01 22:25:56 -0300

    Correção das funções de verificação de PK e FK. Só estavam varrendo a primeira página, agora verificam todas as páginas.

--- Fonte/verificaChaveFK.c
+++ Fonte/verificaChaveFK.c
@@ -11,7 +11,7 @@
    ---------------------------------------------------------------------------------------------*/
 
 int verificaChaveFK(char *nomeTabela,column *c, char *nomeCampo, char *valorCampo, char *tabelaApt, char *attApt){
-    int x,j, erro;
+    int x,j, erro, page;
     char str[20];
     char dat[5] = ".dat";
     struct fs_objects objeto;
@@ -37,37 +37,37 @@ int verificaChaveFK(char *nomeTabela,column *c, char *nomeCampo, char *valorCamp
     for(x = 0; erro == SUCCESS; x++)
         erro = colocaTuplaBuffer(bufferpoll, x, tabela, objeto);
 
-    column *pagina = getPage(bufferpoll, tabela, objeto, 0);
+    for (page = 0; page < PAGES; page++) {
+        column *pagina = getPage(bufferpoll, tabela, objeto, page);
 
-    //printf("asf: %d\n", bufferpoll[0].nrec);
+        for(j = 0; j < objeto.qtdCampos * bufferpoll[0].nrec; j++){
 
-    for(j = 0; j < objeto.qtdCampos * bufferpoll[0].nrec; j++){
+           // printf("VALORC: %s\n VC: %s\n", pagina[j].valorCampo, valorCampo);
 
-       // printf("VALORC: %s\n VC: %s\n", pagina[j].valorCampo, valorCampo);
+            if(strcmp(pagina[j].nomeCampo, attApt) == 0){
 
-        if(strcmp(pagina[j].nomeCampo, attApt) == 0){
+                if(pagina[j].tipoCampo == 'S'){
+                    if(strcmp(pagina[j].valorCampo, valorCampo) == 0){
+                        return SUCCESS;
+                    }
+                } else if(pagina[j].tipoCampo == 'I'){
+                    int *n = (int *)&pagina[j].valorCampo[0];
+                    if(*n == atoi(valorCampo)){
+                        return SUCCESS;
+                    }
+                } else if(pagina[j].tipoCampo == 'D'){
+                    double *nn = (double *)&pagina[j].valorCampo[0];
 
-            if(pagina[j].tipoCampo == 'S'){
-                if(strcmp(pagina[j].valorCampo, valorCampo) == 0){
-                    return SUCCESS;
+                    if(*nn == atof(valorCampo)){
+                        return SUCCESS;
+                    }
+                } else if(pagina[j].tipoCampo == 'C'){
+                    if(pagina[j].valorCampo == valorCampo){
+                        return SUCCESS;
+                    }
+                } else {
+                    return ERRO_CHAVE_ESTRANGEIRA;
                 }
-            } else if(pagina[j].tipoCampo == 'I'){
-                int *n = (int *)&pagina[j].valorCampo[0];
-                if(*n == atoi(valorCampo)){
-                    return SUCCESS;
-                }
-            } else if(pagina[j].tipoCampo == 'D'){
-                double *nn = (double *)&pagina[j].valorCampo[0];
-
-                if(*nn == atof(valorCampo)){
-                    return SUCCESS;
-                }
-            } else if(pagina[j].tipoCampo == 'C'){
-                if(pagina[j].valorCampo == valorCampo){
-                    return SUCCESS;
-                }
-            } else {
-                return ERRO_CHAVE_ESTRANGEIRA;
             }
         }
     }
--- Fonte/verificaChavePK.c
+++ Fonte/verificaChavePK.c
@@ -10,54 +10,62 @@
                 ERRO_CHAVE_PRIMARIA
    ---------------------------------------------------------------------------------------------*/
 
-int verificaChavePK(char *nomeTabela, column *c, char *nomeCampo, char *valorCampo){
-    int j, x, erro;
+int verificaChavePK(char *nomeTabela, column *c, char *nomeCampo, char *valorCampo) {
+    printf("in\n");
+    int j, x, erro, page;
 
     struct fs_objects objeto;
     tp_table *tabela;
     tp_buffer *bufferpoll;
 
     erro = existeAtributo(nomeTabela, c);
-    if(erro != SUCCESS )
+    if (erro != SUCCESS )
         return ERRO_DE_PARAMETRO;
 
 
-    if(iniciaAtributos(&objeto, &tabela, &bufferpoll, nomeTabela) != SUCCESS)
+    if (iniciaAtributos(&objeto, &tabela, &bufferpoll, nomeTabela) != SUCCESS)
         return ERRO_DE_PARAMETRO;
 
+    printf("1\n");
     erro = SUCCESS;
     for(x = 0; erro == SUCCESS; x++)
         erro = colocaTuplaBuffer(bufferpoll, x, tabela, objeto);
 
-    column *pagina = getPage(bufferpoll, tabela, objeto, 0);
+    page = 0;
+    for (page = 0; page < PAGES; page++) {
+        column *pagina = getPage(bufferpoll, tabela, objeto, page);
 
-    for(j = 0; j < objeto.qtdCampos * bufferpoll[0].nrec; j++){
+        for(j = 0; j < objeto.qtdCampos * bufferpoll[0].nrec; j++){
 
-        if(strcmp(pagina[j].nomeCampo, nomeCampo) == 0){
+            if (strcmp(pagina[j].nomeCampo, nomeCampo) == 0) {
+                printf("Verificando campo: %s\n", nomeCampo);
 
-            if(pagina[j].tipoCampo == 'S'){
-                if(strcmp(pagina[j].valorCampo, valorCampo) == 0){
-                    return ERRO_CHAVE_PRIMARIA;
-                }
-            } else if(pagina[j].tipoCampo == 'I'){
-                int *n = (int *)&pagina[j].valorCampo[0];
+                if (pagina[j].tipoCampo == 'S') {
+                    if (strcmp(pagina[j].valorCampo, valorCampo) == 0){
+                        return ERRO_CHAVE_PRIMARIA;
+                    }
+                } else if (pagina[j].tipoCampo == 'I') {
+                    int *n = (int *)&pagina[j].valorCampo[0];
 
-                if(*n == atoi(valorCampo)){
-                    return ERRO_CHAVE_PRIMARIA;
-                }
-            } else if(pagina[j].tipoCampo == 'D'){
-                double *nn = (double *)&pagina[j].valorCampo[0];
+                    printf("Comparando: %d == %d\n", *n, atoi(valorCampo));
+                    if (*n == atoi(valorCampo)) {
+                        return ERRO_CHAVE_PRIMARIA;
+                    }
+                } else if (pagina[j].tipoCampo == 'D'){
+                    double *nn = (double *)&pagina[j].valorCampo[0];
 
-                if(*nn == atof(valorCampo)){
-                    return ERRO_CHAVE_PRIMARIA;
-                }
-            } else if(pagina[j].tipoCampo == 'C'){
-                if(pagina[j].valorCampo == valorCampo){
-                    return ERRO_CHAVE_PRIMARIA;
+                    if (*nn == atof(valorCampo)){
+                        return ERRO_CHAVE_PRIMARIA;
+                    }
+                } else if (pagina[j].tipoCampo == 'C'){
+                    if (pagina[j].valorCampo == valorCampo){
+                        return ERRO_CHAVE_PRIMARIA;
+                    }
                 }
             }
         }
     }
 
+    printf("out\n");
     return SUCCESS;
 }
