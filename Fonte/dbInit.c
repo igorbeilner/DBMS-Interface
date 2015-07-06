aea8065d3a4e95d2ae86a4447b984be490c41152
Regis Thiago Feyh <registhiagofeyh@gmail.com>
2015-07-05 22:32:11 -0300

    Correção para não mostrar mensagem quando pasta já existir.

--- Fonte/dbInit.c
+++ Fonte/dbInit.c
@@ -1,7 +1,7 @@
 #include "buffend.h"
 
 void dbInit() {
-	if(system("mkdir data") == -1)
+	if(system("mkdir data > /dev/null 2>&1") == -1)
 		printf("ERROR: It was not possible to initialize the ibtres\n");;
 	createDB("ibetres");
 }
